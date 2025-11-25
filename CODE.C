d0a538ad0542d301f1f003b6ae8dec82b1cd3c1f
Shreyash111024 <patilshreyash1159@gmail.com>
2025-11-25 19:32:43 +0530

    Refactor program to manage stock data instead of goals
    The program now allows users to add and view stock data instead of goals. It includes file handling for storing and retrieving stock information.

--- CODE.C
+++ CODE.C
@@ -1,29 +1,62 @@
 #include <stdio.h>
 
-
-int main()
-{
-    char goals[1000]= ("Objective 1: Run a 5K race.\nObjective 2: Get 9+ CGPA.");
+int main() {
+    FILE *fpr;
+    char stock[30];
+    float price;
+    int quan;
     int choice;
-    printf("Enter Either You want to write or read the Goals\n Enter 1 for Write or 2 for Read\n Enter choice:");
-    scanf("%d",&choice);
-    if(choice==1){
-    FILE *file;
-    file = fopen("goals.txt", "w");  
-    fprintf(file, "%s", goals); 
-    fclose(file);  
-    }
-    
-    else if(choice==2){
-    FILE *file;
-    file = fopen("goals.txt", "r");  
-    fscanf(file, "%s", goals); 
-    fclose(file);  
-    }
-    else{
-        printf("invalid input");
-    }
 
+    printf("Choose option:\n");
+    printf("1. Write(Add stock)\n");
+    printf("2. Read(View all stocks)\n");
+    printf("Enter choice: ");
+    scanf("%d", &choice);
+
+    if (choice == 1) {
+        fpr = fopen("stocks.txt", "a");
+
+        if (fpr == NULL) {
+            printf("Error opening file!\n");
+            return 0;
+        }
+
+        printf("Enter stock name: ");
+        scanf("%s", stock);
+
+        printf("Enter quantity: ");
+        scanf("%d", &quan);
+
+        printf("Enter price: ");
+        scanf("%f", &price);
+
+        float total = price * quan;
+        printf("Total price: %.2f\n", total);
+
+        fprintf(fpr, "Stock: %s\nQuantity: %d\nPrice: %.2f\nTotal Price: %.2f\n\n",stock, quan, price, total);
+
+        fclose(fpr);
+
+        printf("Stock data saved successfully.\n");
+    }
+    else if (choice == 2) {
+    
+        fpr = fopen("stocks.txt", "r");
+
+        if (fpr == NULL) {
+            printf("Error opening file for reading! File may not exist.\n");
+            return 0;
+        }
+
+        printf("\n--- Saved Stock Details ---\n\n");
+
+        
+
+        fclose(fpr);
+    }
+    else {
+        printf("Invalid choice!\n");
+    }
 
     return 0;
 }
